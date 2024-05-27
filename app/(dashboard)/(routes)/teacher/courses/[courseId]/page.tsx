e64d7761a46087f1df97ed3925abc62496df0443
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-05-28 00:13:36 +0100

    Course Edit page in progress

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -17,13 +17,40 @@ const CourseIdPage = async ({ params }:
         }
     })
 
-    if (!course){
+    if (!course) {
         return redirect("/")
     }
 
+    const requiredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId,
+    ];
+
+    const totalFields = requiredFields.length;
+    const completedFields = requiredFields.filter(Boolean).length
+
+    const completionText = `(${completedFields}/${totalFields})`
     return (
-        <div>
-            Course Id: {params.courseId}
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-medium">
+                        Course Setup
+                    </h1>
+                    <span className="text-sm text-slate-700">Complete all fields {completionText}</span>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <h2 className="text-xl">Customize your course</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
