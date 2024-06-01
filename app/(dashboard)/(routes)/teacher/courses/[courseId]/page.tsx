18295547ffe8a683bec0a66beaa343429cf6dbf7
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-06-01 22:13:50 +0100

    Uploadthing installation in progress

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -48,7 +48,7 @@ const CourseIdPage = async ({ params }:
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
+            <div className="grid grid-cols-1 md:grid-cols-2  gap-4 mt-16">
                 <div>
                     <div className="flex items-center gap-x-2">
                         <IconBadge size="sm" variant="default" icon={LayoutList} />
