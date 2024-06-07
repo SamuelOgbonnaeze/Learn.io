513c6b8792621aa5cb9546557c0eb798df9b31cc
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-06-07 23:16:09 +0100

    Combobox added to category form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -7,6 +7,7 @@ import { IconBadge } from "@/components/icon-badge";
 import TitleForm from "./_components/title-form";
 import DescriptionForm from "./_components/description-form";
 import ImageForm from "./_components/image-form";
+import CategoryForm from "./_components/category-form";
 
 const CourseIdPage = async ({ params }:
     { params: { courseId: string } }
@@ -26,6 +27,13 @@ const CourseIdPage = async ({ params }:
     if (!course) {
         return redirect("/")
     }
+
+    const categories = await prismadb.category.findMany({
+        orderBy: {
+            name: "asc"
+        }
+    })
+
 
     const requiredFields = [
         course.title,
@@ -59,6 +67,12 @@ const CourseIdPage = async ({ params }:
                     <TitleForm initialData={course} courseId={course.id} />
                     <DescriptionForm initialData={course} courseId={course.id} />
                     <ImageForm initialData={course} courseId={course.id} />
+                    <CategoryForm initialData={course} courseId={course.id}
+                        options={categories.map((category) => ({
+                            label: category.name,
+                            value: category.id
+                        }))}
+                    />
                 </div>
             </div>
         </div>
